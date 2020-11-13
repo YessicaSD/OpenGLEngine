53965ff7b0bb0768f21080f6d18a8797cd7fa1f2
Yessica Servin Dominguez <36170847+YessicaSD@users.noreply.github.com>
2020-11-13 22:34:24 +0100

    adding premake to the project

---
+++ premake5.lua
@@ -0,0 +1,99 @@
+workspace "Shadow"
+    architecture "x64"
+   configurations { "Debug", "Release", "Dist"}
+
+   outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+project "Shadow"
+    location "Shadow"
+    kind "sharedLib"
+    language "C++"
+
+   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+   objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+   
+   files 
+   { 
+       "%{prj.name}/src/**.h",
+       "%{prj.name}/src/**.cpp",
+   }
+
+   includedirs
+   {
+        "%{prj.name}/vendor/spdlog/include",
+   }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.18362.0"
+        defines 
+        { 
+            "SW_PLATFORM_WINDOWS",
+            "SW_BUILD_DLL"
+        }
+    
+    postbuildcommands
+    {
+        ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir.. "/Sandbox")
+	}
+
+   filter "configurations:Debug"
+      defines "SW_DEBUG"
+      symbols "On"
+
+   filter "configurations:Release"
+      defines "SW_RELEASE"
+      optimize "On"
+
+   filter "configurations:Dist"
+      defines "SW_DIST"
+      optimize "On"
+
+ project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+   
+
+     files 
+   { 
+       "%{prj.name}/src/**.h",
+       "%{prj.name}/src/**.cpp",
+   }
+
+   includedirs 
+   {
+        "Shadow/vendor/spdlog/include",
+        "Shadow/src"
+   }
+
+   links 
+   {
+       "Shadow"
+   }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.18362.0"
+        defines 
+        { 
+            "SW_PLATFORM_WINDOWS" 
+        }
+    
+   filter "configurations:Debug"
+      defines "SW_DEBUG"
+      symbols "On"
+
+   filter "configurations:Release"
+      defines "SW_RELEASE"
+      optimize "On"
+
+   filter "configurations:Dist"
+      defines "SW_DIST"
+      optimize "On"
+
+   
